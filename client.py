11794d406c826328e0e2b42181447749c376db8d
Benjamin Denzler <bdenz001@ucr.edu>
2023-05-21 12:59:06 -0600

    Added choice of model and some error handling
    User can now specify which model they want to use for image recognition, incorrect models are rejected. Added some error handling on the client POST requests.

--- client.py
+++ client.py
@@ -16,13 +16,20 @@ if not os.path.exists('imagenet-classes.txt'):
     print("Downloading ImageNet classes file...")
     subprocess.run(['wget', 'https://raw.githubusercontent.com/xmartlabs/caffeflow/master/examples/imagenet/imagenet-classes.txt'])
 
+model_list = dir(models)
+user_model = input("Choose a model: ")
+while user_model not in model_list:
+    user_model = input("That's not a valid model, please try again: ")
+print(f"Sending request with model {user_model}...")
+
 # Send POST request to the `predict` endpoint
 url = 'http://localhost:5000/predict'
 files = {'image': ('dog.jpg', open('pics/dog.jpg', 'rb'), 'image/jpeg')}
-response = requests.post(url, files=files)
-
-if response.ok:
-    result = response.json()
-    print(f'Predicted class: {result["class"]}, confidence: {result["confidence"]}')
-else:
-    print(f'Error: {response.status_code} - {response.text}')
+try:
+    response = requests.post(url, files=files)
+    if response.ok:
+        result = response.json()
+        print(f'Predicted class: {result["class"]}, confidence: {result["confidence"]}')
+except requests.exceptions.RequestException as e:
+    print("An error occurred while sending the request!")
+    print(f"Error: {str(e)}")
