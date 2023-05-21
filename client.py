480a187c12f823a9803302ac5ece5b99221a8d38
Benjamin Denzler <bdenz001@ucr.edu>
2023-05-21 17:18:47 -0600

    Print list of models for the user

--- client.py
+++ client.py
@@ -6,21 +6,22 @@ from torchvision import models
 # TODO: Client should let user specify the image that is sent for recognition
 # It's probably fine to just download a set of images in the `pics` folder and let the user choose from those
 
-# TODO: Client should let user specify the model the server uses for image recognition
-# The command `print(dir(models))` prints all the models
-# User probably doesn't need to choose from every one of them
-# We can identify the most interesting models and provide those as options
-
 # Download imagenet-classes.txt if it doesn't exist
 if not os.path.exists('imagenet-classes.txt'):
     print("Downloading ImageNet classes file...")
     subprocess.run(['wget', 'https://raw.githubusercontent.com/xmartlabs/caffeflow/master/examples/imagenet/imagenet-classes.txt'])
 
+# Print the models the user can choose from
 model_list = dir(models)
+model_list = [str(model) for model in model_list]
+model_list = ', '.join(model_list)
+print(f"MODELS: {model_list}\n")
+
+# Get user choice of model
 user_model = input("Choose a model: ")
 while user_model not in model_list:
     user_model = input("That's not a valid model, please try again: ")
-print(f"Sending request with model {user_model}...")
+print(f"\nSending request with model {user_model}...")
 
 # Send POST request to the `predict` endpoint
 url = 'http://localhost:5000/predict'
