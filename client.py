99a1f099f39b7ee8c7d142543a25c64a727bdfb3
Benjamin Denzler <bdenz001@ucr.edu>
2023-05-21 17:35:48 -0600

    Added choice of picture

--- client.py
+++ client.py
@@ -6,6 +6,12 @@ from torchvision import models
 # TODO: Client should let user specify the image that is sent for recognition
 # It's probably fine to just download a set of images in the `pics` folder and let the user choose from those
 
+# Prints a list as comma-seperated elements
+def list_to_csv(list):
+    list = [str(element) for element in list]
+    list = ', '.join(list)
+    return list
+
 # Download imagenet-classes.txt if it doesn't exist
 if not os.path.exists('imagenet-classes.txt'):
     print("Downloading ImageNet classes file...")
@@ -13,17 +19,25 @@ if not os.path.exists('imagenet-classes.txt'):
 
 # Print the models the user can choose from
 model_list = dir(models)
-model_list = [str(model) for model in model_list]
-model_list = ', '.join(model_list)
-print(f"MODELS: {model_list}\n")
+print(f"MODELS: {list_to_csv(model_list)}")
 
 # Get user choice of model
-user_model = input("Choose a model: ")
+user_model = input("\nChoose a model: ")
 while user_model not in model_list:
     user_model = input("That's not a valid model, please try again: ")
 print(f"\nSending request with model {user_model}...")
 
+# Print the pics the user can choose from
+pic_list = os.listdir('./pics')
+print(f"PICS: {list_to_csv(pic_list)}")
+
+# Get user choice of pic
+user_pic = input("\nChoose a pic: ")
+while user_pic not in pic_list:
+    user_pic = input("That's not a valid pic, please try again: ")
+
 # Send POST request to the `predict` endpoint
+print(f"\nSending request for pic {user_pic} with model {user_model}...")
 url = 'http://localhost:5000/predict'
 files = {'image': ('dog.jpg', open('pics/dog.jpg', 'rb'), 'image/jpeg')}
 try:
