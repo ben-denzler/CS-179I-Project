79f81bc2dd08dc23ca605ba708676736aa8d0459
Benjamin Denzler <bdenz001@ucr.edu>
2023-05-21 19:04:22 -0600

    Removed code related to choosing model

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import subprocess
 import os
 import statistics
 from time import time
-from torchvision import models
 
 SERVER_URL = 'http://localhost:5000/predict'
 CLASSES_URL = 'https://raw.githubusercontent.com/xmartlabs/caffeflow/master/examples/imagenet/imagenet-classes.txt'
@@ -33,39 +32,25 @@ if not os.path.exists(CLASSES_FILE):
     print("Downloading ImageNet classes file...")
     subprocess.run(['wget', CLASSES_URL])
 
-"""
-# TODO: Need to only list models that can be called in the server
-# Print the models the user can choose from
-model_list = dir(models)
-print(f"MODELS: {list_to_csv(model_list)}")
-
-# Get user choice of model
-user_model = input("\nChoose a model: ")
-while user_model not in model_list:
-    user_model = input("That's not a valid model, please try again: ")
-print(f"\nSending request with model {user_model}...")
-"""
-
 # Print the pics the user can choose from
 pic_list = os.listdir(PICS_DIR)
 print(f"PICS: {list_to_csv(pic_list)}")
 
 # Get user choice of pic
-user_pic = input("\nChoose a pic: ")
+user_pic = input("Choose a pic: ")
 while user_pic not in pic_list:
     user_pic = input("That's not a valid pic, please try again: ")
 
 # Get number of requests
-print("\nChoose the number of requests to send: ", end='')
+print("Choose the number of requests to send: ", end='')
 num_requests = get_positive_integer()
 
 pic_path = PICS_DIR + '/' + user_pic
 execution_times = []
-files = {'image': (user_pic, open(pic_path, 'rb'), 'image/jpeg')}
-# data = {'model': user_model}
 
 # Send POST request to the server
 for i in range(num_requests):
+    files = {'image': (user_pic, open(pic_path, 'rb'), 'image/jpeg')}
     print(f"\nSending request {i+1} for pic {user_pic}...")
     try:
         start_time = time()
@@ -79,7 +64,7 @@ for i in range(num_requests):
         else:
             print(f"An error occurred with code: {response.status_code}")
     except requests.exceptions.RequestException as e:
-        print("An error occurred while sending the request!")
+        print("An exception occurred while sending the request!")
         print(f"Error: {str(e)}")
         exit(1)
 
