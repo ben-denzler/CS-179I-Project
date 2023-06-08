bc7eb9ce9d432004feb3101d6aef277d84c3b6b9
Benjamin Denzler <bdenz001@ucr.edu>
2023-06-08 00:01:13 -0600

    Fixed locustfile, Locust load testing is usable

--- locustfile.py
+++ locustfile.py
@@ -1,28 +1,28 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, constant
 
 """
-This file is intended to be used with the load tester Locust.
-This is for testing.
+Configuration for Locust load tester, see https://docs.locust.io/
 """
 
-HOST_NAME = 'cs179i-project.default.example.com'
-PICS_DIR = './pics'
+host_name = 'cs179i-project.default.example.com'
+pics_dir = './pics'
 pic = "strawberries.jpg"
 model = "AlexNet"
 
 class MyUser(HttpUser):
-    wait_time = between(1, 3)  # Wait time between each task execution
-    host = 'http://128.110.218.63:32676/predict'
+    wait_time = constant(0)  # Wait time between each task execution
+    host = 'http://128.110.218.63:32676'
 
     @task
     def send_request(self):
-        pic_path = PICS_DIR + '/' + pic
+        pic_path = pics_dir + '/' + pic
         data = {'model': model}
         files = {'image': (pic, open(pic_path, 'rb'), 'image/jpeg')}
-        headers = {'Host': HOST_NAME}
+        headers = {'Host': host_name}
 
+        # Send POST request with fields "data, files, headers" to "host"
         with self.client.post("/predict", data=data, files=files, headers=headers, catch_response=True) as response:
             if response.status_code == 200:
                 response.success()
             else:
-                response.failure("Request failed")
+                response.failure(f"Request failed with status code: {response.status_code}")
